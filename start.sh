a8dbc870196ab5c232b3cc265a0b87c019ab31ce
herrysis404 <67797468+herrysis404@users.noreply.github.com>
2020-10-10 23:43:31 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-# configs
-mkdir -p /etc/caddy/ /usr/share/caddy && wget $CADDYIndexPage -O /usr/share/caddy/index.html 
-unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-[[ "$ROBOTS" == "true" ]] && wget -qO- $ROBOTSCONFIG >/usr/share/caddy/robots.txt
-wget -qO- $CADDYCONFIG | sed -e "1c :$PORT" -e "s/\$SSPATH$/\\$SSPATH/" -e "s/\$GOSTPATH$/\\$GOSTPATH/" -e "s/\$BROOKPATH$/\\$BROOKPATH/" -e "s/\$VMESSPATH$/\\$VMESSPATH/" -e "s/\$VLESSPATH$/\\$VLESSPATH/" >/etc/caddy/Caddyfile
-wget -qO- $V2RAYCONFIG | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$VMESSPATH/\\$VMESSPATH/" -e "s/\$VLESSPATH/\\$VLESSPATH/" >/v2ray.json
+# Install rclone static binary
+curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
 
-# start
-[[ "$TOREnable"      ==    "true" ]]    &&    tor &
+# Install aria2c static binary
+wget -q https://github.com/P3TERX/aria2-builder/releases/download/1.35.0_2020.09.04/aria2-1.35.0-static-linux-amd64.tar.gz
+tar xf aria2-1.35.0-static-linux-amd64.tar.gz
 
-[[ "$V2RAYEnable"    ==    "true" ]]    &&    /v2ray -config /v2ray.json &
+# Install ffmpeg static binary
+wget -q https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
+tar xvf ffmpeg-git-amd64-static.tar.xz
 
-[[ "$BROOKEnable"    ==    "true" ]]    &&    brook wsserver -l 127.0.0.1:3234 --path $BROOKPATH -p $APASSWORD &
+mv rclone-*-linux-amd64/rclone aria2c ffmpeg-*-amd64-static/ffmpeg ffmpeg-*-amd64-static/ffprobe /usr/local/bin/
 
-[[ "$GOSTEnable"     ==    "true" ]]    &&    eval gost $GOSTMETHOD &
+wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
+wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
 
-[[ "$SSEnable"       ==    "true" ]]    &&    ss-server -s 127.0.0.1 -p 1234 -k $APASSWORD -m $SSENCYPT --plugin /usr/bin/v2ray-plugin_linux_amd64 --plugin-opts "server;path=$SSPATH" &
+# Tracker
+bash <(curl -fsSL git.io/tracker.sh) "/app/aria2c.conf"
 
-caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+#Peer Agent
+echo "peer-id-prefix=-qB4250-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!~*()._-' | fold -w 12 | head -n 1)" >> /app/aria2c.conf
